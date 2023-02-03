cd61378ef831aa8954b4051c2602c6c12208660e
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-02-03 09:58:32 +0900

    Update 02_4375_1.py

--- codeplus/코딩 테스트 준비 - 기초/02 챕터) 수학/01_19분_나머지 연산_/02_4375_1.py
+++ codeplus/코딩 테스트 준비 - 기초/02 챕터) 수학/01_19분_나머지 연산_/02_4375_1.py
@@ -1,9 +1,13 @@
 # https://www.acmicpc.net/problem/4375
 
+
+print('===[2023-02-02 (목)]===')
+
 # n = 3
 # n = 7
 # n = 9901
 
+# 컴퓨터는 실수 계산을 못 한다
 # while True:
 #     try:
 #         n = int(input())
@@ -20,26 +24,41 @@
 #         i += 1
 
 
+print("===[내풀이: 160ms]===")
+while True:
+    try:
+        n = int(input())
+    except:
+        break
 
-# 왜 이거만 맞다고 함?
-# while True:
-#     try:
-#         n = int(input())
-#     except:
-#         break
+    i = 1
+    num = 0
+    while True:
+        num = 10 * num + 1
+        if num % n == 0:
+            print(i)
+            break
+        i += 1
 
-#     i = 1
-#     num = 0
-#     while True:
-#         num = 10 * num + 1
-#         if num % n == 0:
-#             print(i)
-#             break
-#         i += 1
+print("===[다른사람풀이: 40ms]===")
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+    num = 0
+    i = 1
+    while True:
+        num = num * 10 + 1
+        num %= n # 요거 두 줄로
+        if num == 0: # 1/4 더 빠른 풀이
+            print(i)
+            break
+        i += 1
 
 
-for i in range(20):
-    i += 1
-    num = (1 / 9) * (10 ** i) - (1 / 9)
-    # num = round(num)
-    print(num, i)
+# for i in range(20):
+#     i += 1
+#     num = (1 / 9) * (10 ** i) - (1 / 9)
+#     num = round(num)
+#     print(num, i)
