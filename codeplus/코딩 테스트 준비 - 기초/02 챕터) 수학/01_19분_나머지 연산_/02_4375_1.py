9cdeee190504a9d2414f1ce65b50593dfcea71dd
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-02-04 17:26:24 +0900

    Update 02_4375_1.py

--- codeplus/코딩 테스트 준비 - 기초/02 챕터) 수학/01_19분_나머지 연산_/02_4375_1.py
+++ codeplus/코딩 테스트 준비 - 기초/02 챕터) 수학/01_19분_나머지 연산_/02_4375_1.py
@@ -24,41 +24,41 @@ print('===[2023-02-02 (목)]===')
 #         i += 1
 
 
-print("===[내풀이: 160ms]===")
-while True:
-    try:
-        n = int(input())
-    except:
-        break
+# print("===[내풀이: 160ms]===")
+# while True:
+#     try:
+#         n = int(input())
+#     except:
+#         break
 
-    i = 1
-    num = 0
-    while True:
-        num = 10 * num + 1
-        if num % n == 0:
-            print(i)
-            break
-        i += 1
+#     i = 1
+#     num = 0
+#     while True:
+#         num = 10 * num + 1
+#         if num % n == 0:
+#             print(i)
+#             break
+#         i += 1
 
-print("===[다른사람풀이: 40ms]===")
-while True:
-    try:
-        n = int(input())
-    except:
-        break
-    num = 0
-    i = 1
-    while True:
-        num = num * 10 + 1
-        num %= n # 요거 두 줄로
-        if num == 0: # 1/4 더 빠른 풀이
-            print(i)
-            break
-        i += 1
+# print("===[다른사람풀이: 40ms]===")
+# while True:
+#     try:
+#         n = int(input())
+#     except:
+#         break
+#     num = 0
+#     i = 1
+#     while True:
+#         num = num * 10 + 1
+#         num %= n # 요거 두 줄로
+#         if num == 0: # 1/4 더 빠른 풀이
+#             print(i)
+#             break
+#         i += 1
 
 
-# for i in range(20):
-#     i += 1
-#     num = (1 / 9) * (10 ** i) - (1 / 9)
-#     num = round(num)
-#     print(num, i)
+for i in range(20):
+    i += 1
+    num = (1 / 9) * (10 ** i) - (1 / 9)
+    num = int(num)
+    print(num)
