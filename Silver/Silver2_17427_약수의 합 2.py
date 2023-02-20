5ca0fccffa207a052827d2bd87a98218753bd1e5
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-02-20 20:07:48 +0900

    Update Silver2_17427_약수의 합 2.py

--- Silver/Silver2_17427_약수의 합 2.py
+++ Silver/Silver2_17427_약수의 합 2.py
@@ -1,5 +1,5 @@
 # https://www.acmicpc.net/problem/17427
-
+# 시간초과
 n = int(input())
 
 g = 0
@@ -7,7 +7,7 @@ for i in range(n):
     i += 1
     f = 0
     for j in range(i):
-        j += 1
+        j += 1 
         if i % j == 0:
             f += j
     g += f
