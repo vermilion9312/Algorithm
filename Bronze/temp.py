722f7900b6022f1bcb3851f8407bbd0f459934c9
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-04-10 17:50:18 +0900

    Create temp.py

---
+++ Bronze/temp.py
@@ -0,0 +1,21 @@
+def solution(n):
+    answer = []
+    return answer
+
+n = 6
+
+pos = {"x": 1, "y": 1}
+num = 1
+
+end = 1 + 2 * n + i * (i - 1) / 2
+for i in range(end):
+    print()
+
+
+# print(pos["x"])
+
+
+'''
+
+
+'''
