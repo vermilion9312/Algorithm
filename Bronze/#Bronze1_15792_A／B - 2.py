764c2370ca605f1d812153abddfb7c35782219f0
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-04-12 17:50:55 +0900

    .

---
+++ Bronze/#Bronze1_15792_A／B - 2.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/15792
+
+
+print("230412-3-1157")
+
+from decimal import Decimal
+
+a, b = map(int, input().split())
+
+result = ""
+
+'''
+1/3
+'''
---
+++ Bronze/Bronze3_2445_별 찍기 - 8.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/2445
+
+print("230412-3-1513")
+
+n = int(input())
+
+h = 2 * n - 1
+w = 2 * n
+
+k = 0
+for i in range(h):
+    if i < h // 2:
+        for j in range(w):
+            if j >= 1 + i and j <= h - 1 - i:
+                print(" ", end="")
+            else:
+                print("*", end="")
+    elif i > h // 2:
+        for j in range(w):
+            if j >= w // 2 - 1 - k and j <= w // 2 + k:
+                print(" ", end="")
+            else:
+                print("*", end="")
+        k += 1
+    else:
+        for j in range(w):
+            print("*", end="")
+    print()
