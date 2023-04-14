9dfb945cc65d5903090cb01d36f1bd5074aa8739
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-04-14 11:30:34 +0900

    .

---
+++ Silver/Silver4_10994_별 찍기 - 19.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/10994
+
+n = 3
+arr = []
+
+def star(n):
+    for i in range(4 * n - 3):
+        arr.append([])
+        for j in range(4 * n - 3):
+            if
+            print("*", end="")
+        print()
+
+star(3)
+
+'''
+1 >> 1
+2 >> 5
+3 >> 9
+4 >> 13
+4n - 3
+'''
