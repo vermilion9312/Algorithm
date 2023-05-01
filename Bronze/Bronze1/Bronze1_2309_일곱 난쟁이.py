b596ae699e59d4daf2a68a13d2c8a6b8e37df03e
vermilion9312 <vermilion9312@gmail.com>
2023-05-01 17:01:43 +0900

    Update Bronze1_2309_일곱 난쟁이.py

--- Bronze/Bronze1/Bronze1_2309_일곱 난쟁이.py
+++ Bronze/Bronze1/Bronze1_2309_일곱 난쟁이.py
@@ -15,70 +15,72 @@ for i in range(8):
                     print(dwarfs[k])
             exit()
 
+import sys
 
+n = 9
+a = [int(input()) for _ in range(n)]
+a.sort()
+for i in range(0, n):
+    for j in range(i + 1, n):
+        total = 0
+        for k in range(0, n):
+            if i == k or j == k:
+                continue
+            total += a[k]
+        if total == 100:
+            for k in range(0, n):
+                if i == k or j == k:
+                    continue
+                print(a[k])
+            sys.exit(0)
 
+print("오답230418-2-1823")
+import sys
 
+total = 0
+arr = []
+for _ in range(9):
+    height = int(input())
+    total += height
+    arr.append(height)
 
+total -= 100
 
-# import sys
+# 두 명 고르기
+arr2 = []
+for i in range(len(arr)):
+    # 키 한 명 걸 빼고
+    total -= arr[i]
 
-# n = 9
-# a = [int(input()) for _ in range(n)]
-# a.sort()
-# for i in range(0, n):
-#     for j in range(i + 1, n):
-#         total = 0
-#         for k in range(0, n):
-#             if i == k or j == k:
-#                 continue
-#             total += a[k]
-#         if total == 100:
-#             for k in range(0, n):
-#                 if i == k or j == k:
-#                     continue
-#                 print(a[k])
-#             sys.exit(0)
+    for j in range(len(arr)):
+        # 둘은 다르고
+        if i != j:
+            # 키 한 명 걸 빼고
+            total -= arr[j]
+        
+            if total == 0:
+                arr2.append(arr[i])
+                arr2.append(arr[j])
+                break
+            #원복
+            else:
+                total += arr[j]
+    #원복
+    if total == 0:
+        break
+    else:
+        total += arr[i]
 
+# 10명 구하기
+arr3 = []
+for v in arr:
+    if v not in arr2:
+        arr3.append(v)
 
-# print("오답230418-2-1823")
-# total = 0
-# arr = []
-# while True:
-#     try:
-#         height = int(input())
-#         total += height
-#         arr.append(height)
-#     except:
-#         break
+arr3.sort()
 
-# total -= 100
-# arr2 = []
+for v in arr3:
+    print(v)
 
-# for i in range(len(arr)):
-#     total -= arr[i]
+sys.exit(0)
 
-#     for j in range(len(arr)):
-#         if i != j:
-#             total -= arr[j]
-#             if total == 0:
-#                 arr2.append(arr[i])
-#                 arr2.append(arr[j])
-#                 break
-#             else:
-#                 total += arr[j]
-
-#     if total == 0:
-#         break
-#     else:
-#         total += arr[i]
-
-# arr3 = []
-
-# for v in arr:
-#     if v not in arr2:
-#         arr3.append(v)
-
-# arr3.sort()
-
-# for v in arr3:
-#     print(v)
