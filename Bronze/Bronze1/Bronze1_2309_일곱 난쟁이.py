6375bf699221b78a4969ed12b2cab68b082d0705
vermilion9312 <vermilion9312@gmail.com>
2023-05-01 17:49:32 +0900

    Update Bronze1_2309_일곱 난쟁이.py

--- Bronze/Bronze1/Bronze1_2309_일곱 난쟁이.py
+++ Bronze/Bronze1/Bronze1_2309_일곱 난쟁이.py
@@ -1,41 +1,6 @@
 # https://www.acmicpc.net/problem/2309
 
-n = 9
-dwarfs = []
-for i in range(9):
-    dwarfs.append(int(input()))
-
-dwarfs_sum = sum(dwarfs)
-
-for i in range(8):
-    for j in range(i + 1, 9):
-        if dwarfs_sum - dwarfs[i] - dwarfs[j] == 100:
-            for k in range(9):
-                if k != i and k != j:
-                    print(dwarfs[k])
-            exit()
-
-import sys
-
-n = 9
-a = [int(input()) for _ in range(n)]
-a.sort()
-for i in range(0, n):
-    for j in range(i + 1, n):
-        total = 0
-        for k in range(0, n):
-            if i == k or j == k:
-                continue
-            total += a[k]
-        if total == 100:
-            for k in range(0, n):
-                if i == k or j == k:
-                    continue
-                print(a[k])
-            sys.exit(0)
-
 print("오답230418-2-1823")
-import sys
 
 total = 0
 arr = []
@@ -45,42 +10,29 @@ for _ in range(9):
     arr.append(height)
 
 total -= 100
+restore = total
 
-# 두 명 고르기
-arr2 = []
 for i in range(len(arr)):
-    # 키 한 명 걸 빼고
-    total -= arr[i]
-
     for j in range(len(arr)):
-        # 둘은 다르고
-        if i != j:
-            # 키 한 명 걸 빼고
-            total -= arr[j]
-        
+         if i != j:
+            total -= (arr[i] + arr[j])
             if total == 0:
-                arr2.append(arr[i])
-                arr2.append(arr[j])
+                idx1 = i
+                idx2 = j
                 break
-            #원복
             else:
-                total += arr[j]
-    #원복
-    if total == 0:
-        break
-    else:
-        total += arr[i]
+                total = restore
+                
+del(arr[idx1])
+del(arr[idx2])
 
-# 10명 구하기
-arr3 = []
+arr.sort()
+
 for v in arr:
-    if v not in arr2:
-        arr3.append(v)
-
-arr3.sort()
-
-for v in arr3:
     print(v)
 
-sys.exit(0)
+
+
+
+
 
