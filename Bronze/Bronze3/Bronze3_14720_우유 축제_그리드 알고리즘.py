8172e83ad44d6e5ab8f8c68d7beb9b51274e4dea
vermilion9312 <vermilion9312@gmail.com>
2023-05-02 19:53:44 +0900

    230502-2-1953

---
+++ Bronze/Bronze3/Bronze3_14720_우유 축제_그리드 알고리즘.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/14720
+
+print("230502-2-1947")
+
+n = int(input())
+store = list(map(int, input().split()))
+
+milkList = [0, 1, 2]
+
+count = 0
+idx = 0
+# 매장 마다 우유를 마주쳤을 때
+for milk in store:
+    # 마주친 우유가 딸기 우유라면
+    if milk == milkList[idx]:
+        # 마신 카운트를 올리고
+        count += 1
+        # 그 다음 우유를 선택
+        idx += 1
+        # 근데 index 가 0, 1, 2 반복해야 하니깐 3으로 나눈 나머지 구하기
+        idx %= 3
+    # else:
+    #     continue 굳이 없어도 되는 부분 어차피 아니면 다시 올라감
+
+print(count)
+
