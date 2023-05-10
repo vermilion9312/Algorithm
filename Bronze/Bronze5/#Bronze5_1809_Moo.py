48b4c9406b69d101aeea43b783cc39777ef42990
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-05-10 23:27:48 +0900

    230510-3-2327

---
+++ Bronze/Bronze5/#Bronze5_1809_Moo.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1809
+
+print("")
+
+print("(___)")
+print("(o o)____/")
+print(" @@      \\")
+print("  \ ____,/")
+print("  //   //")
+print(" ^^   ^^")
---
+++ Bronze/Bronze5/Bronze5_2338_긴자리 계산.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/2338
+
+print("230510-3-2327")
+
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
