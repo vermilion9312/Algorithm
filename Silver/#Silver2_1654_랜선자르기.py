791cadd0759a817be81781dd44579397021ba446
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-06-17 10:42:09 +0900

    .

--- Silver/#Silver2_1654_랜선자르기.py
+++ Silver/#Silver2_1654_랜선자르기.py
@@ -2,27 +2,71 @@ k, n = map(int, input().split())
 arr = [int(input()) for _ in range(k)]
 
 
-# 시간초과
-# k = 2
-# n = 10
-
-# arr = [100, 120]
-
-# print(sum(arr))
-
-length = sum(arr) // n
-
-
 while True:
-
-    count = 0
     for v in arr:
-        count += v // length
-    
-    if count >= n:
-        break
-    else:
-        count = 0
-        length -= 1
+        low = 1
+        high = v
 
-print(length)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# k, n = map(int, input().split())
+# arr = [int(input()) for _ in range(k)]
+
+
+# # 시간초과
+# # k = 2
+# # n = 10
+
+# # arr = [100, 120]
+
+# # print(sum(arr))
+
+# length = sum(arr) // n
+
+
+# while True:
+
+#     count = 0
+#     for v in arr:
+#         count += v // length
+    
+#     if count >= n:
+#         break
+#     else:
+#         count = 0
+#         length -= 1
+
+# print(length)
