5a5134197a38f8571b56f628ef82b67a88b9eec6
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-07-02 09:24:46 +0900

    Create Silver4_1920_수 찾기.py

---
+++ Silver/Silver4_1920_수 찾기.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/1920
+
+import math
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# m = int(input())
+# targetArr = list(map(int, input().split()))
+
+
+# arr = sorted(arr)
+
+# for target in targetArr:
+#     left = arr[0]
+#     right = arr[-1]
+#     while left < right:
+#         mid = math.floor((left + right) / 2)
+#         if target > mid:
+#             left = mid
+#         elif target < mid:
+#             right = mid
+#         else:
+print(round(3.5))
