67495f837016e5c606d01d95878c31f2985d58ab
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-07-07 11:19:07 +0900

    [Bronze I] Title: 단어 공부, Time: 3264 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,19 @@
+string = input().upper()
+arr = list(set(string))
+count = [0 for _ in range(len(arr))]
+
+for i in range(len(arr)):
+    for j in range(len(string)):
+        if arr[i] == string[j]:
+            count[i] += 1
+
+maxNum = max(count)
+check = 0
+for i in range(len(count)):
+    if maxNum == count[i]:
+        check += 1
+        idx = i
+if check >= 2:
+    print("?")
+else:
+    print(arr[idx])
