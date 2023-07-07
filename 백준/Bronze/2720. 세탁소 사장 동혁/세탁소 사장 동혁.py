03d396f7c83db6dacf2fcad0c8a4f17da1543030
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-07-07 11:27:33 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,9 @@
+charge = [25, 10, 5, 1]
+
+T = int(input())
+for _ in range(T):
+    C = int(input())
+    for cv in charge:
+        count = C // cv
+        C %= cv
+        print(int(count), end = " ")
