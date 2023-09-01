aecc04dfd04b1ab44ac346d7c3922875fd130148
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-09-01 14:27:32 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 64 ms, Memory: 34568 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -1,14 +1,35 @@
+# 1차적으로 풀었던 것
+# mountainCount = int(input())
+# mountain = list(map(int, input().split()))
+
+# killCountList = []
+# for i in range(mountainCount):
+#     killCount = 0
+#     for j in range(mountainCount):
+#         if i < j and mountain[i] > mountain[j]:
+#             killCount += 1
+#             killCountList.append(killCount)
+#         if mountain[i] < mountain[j]:
+#             break
+
+# print(killCountList)
+# print(max(killCountList))
+
 mountainCount = int(input())
-mountain = list(map(int, input().split()))
+mountainList = list(map(int, input().split()))
 
-countList = []
-for i in range(mountainCount):
-    killCount = 0
-    for j in range(mountainCount):
-        if i < j and mountain[i] > mountain[j]:
-            killCount += 1
-            countList.append(killCount)
-        if mountain[i] < mountain[j]:
-            break
+lastKillCount = 0
+highestMountain = 0
+killCount = 0
 
-print(max(countList))
+for mountain in mountainList:
+    if mountain > highestMountain:
+        highestMountain = mountain
+
+        lastKillCount = 0
+    else:
+        lastKillCount += 1
+
+    killCount = max(lastKillCount, killCount)
+
+print(killCount)
