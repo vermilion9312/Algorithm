9d7f44645ff4979e4125821470bbcdc1d09d8bd3
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-09-06 02:35:16 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,19 +1,6 @@
 n = int(input())
 
-length = 2 * n - 1
-m = 0
-for i in range(length):
-    if i < length // 2:
-        for j in range(n + i):
-            if j < n - 1 - i:
-                print(" ", end="")
-            else:
-                print("*", end="")
-    else:
-        for k in range(length - i + length // 2):
-            if k < m:
-                print(" ", end="")
-            else:
-                print("*", end="")
-        m += 1
-    print()
+for i in range(n):
+    print(' ' * (n - 1 - i) + '*' * (2 * i + 1))
+for i in range(n - 1):
+    print(' ' * (i + 1) + '*' * (2 * (n - i - 1) - 1))
