d1eeb563138b8e9c4a9318d709b681d18d30c699
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-09-07 04:14:27 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.py
@@ -2,15 +2,6 @@ n = int(input())
 
 for i in range(n):
     if i % 2 == 0:
-        for j in range(2 * n - 1):
-            if j % 2 == 0:
-                print("*", end="")
-            else:
-                print(" ", end="")
+        print("* " * n)
     else:
-        for j in range(2 * n):
-            if j % 2 == 0:
-                print(" ", end="")
-            else:
-                print("*", end="")
-    print()
+        print(" *" * n)
