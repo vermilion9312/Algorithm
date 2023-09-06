762b84ec38216067c3ac043cbb4fe55299c5fde8
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-09-07 00:53:35 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.py
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.py
@@ -1,5 +1,5 @@
 n = int(input())
 
-print(' ' * (n - 1) + '*')
+print(' '*(n-1)+'*')
 for i in range(1, n):
-    print(' ' * (n - 1 - i) + '*' + ' ' * (2 * i - 1) + '*')
+    print(' '*(n-1-i)+'*'+' '*(2*i-1)+'*')
