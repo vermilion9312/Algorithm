f95b724fd1cc4e43a044ae227adfc4af48b7a467
이주호 <83685855+vermilion9312@users.noreply.github.com>
2023-09-08 15:28:08 +0900

    [Silver III] Title: 1, Time: 160 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+
+    i = 1
+    num = 0
+    while True:
+        num = 10 * num + 1
+        if num % n == 0:
+            print(i)
+            break
+        i += 1
